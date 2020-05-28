1925b88e89312b642cf95dc144ab8a9ed5ffb3f7
SabeetHossain <66021093+SabeetHossain@users.noreply.github.com>
2020-05-27 22:50:09 -0400

    Update SystemFact.java

--- SystemFact.java
+++ SystemFact.java
@@ -14,15 +14,31 @@ public class SystemFact
   public static void setThing()
   {
     String a;
+    String b;
+    String Password;
     System.out.println("What is the name of this website");
     a=myScanner.nextLine();
     setWebsite(a);
-    System.out.println("Do you already have a password for this website");
+    System.out.println("Do you already have a password for this website(y/n)");
+    b=myScanner.nextLine();
+    if(b=="y")
+    {
+     //end generation of this password save users password and moves on to the next site
+     System.out.println("What is the password you are using?");
+     Password=myScanner.nextLine();
+    }
+    else if(b="n")
+    {
+    //continue password generating
+      
+    }
   }
+ 
   public static void setWebsite(String aname)
   {
     websiteName=aname;
-  } 
+  }
+
   public static String getWebsite()
   {
     return websiteName;
